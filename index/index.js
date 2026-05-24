6c6b312302d6f97d1d98089009ee1a7ea5740009
Ristoc <114589831+BerryStraw-01@users.noreply.github.com>
2026-05-24 16:56:21 +0900

    indexのボタン感触の削除

--- index/index.js
+++ index/index.js
@@ -51,12 +51,6 @@ function show(id){
    参加
 ====================== */
 document.getElementById("btnJoin").onclick = async ()=>{
-
-
-  if ("vibrate" in navigator) {
-      navigator.vibrate(10);
-    }
-
   if(!currentState) return;
 
   const name = document.getElementById("name").value;
@@ -422,13 +416,6 @@ onSnapshot(stateRef, async (snap) => {
    回答
 ====================== */
 window.answer = async (i)=>{
-
-
-  // ✅ 押下時の触覚フィードバック
-  if ("vibrate" in navigator) {
-    navigator.vibrate(10);
-  }
-
   if(hasAnswered) return;
 
   hasAnswered = true;
